b9a9725005334de6b907dca6de1805634b7d3030
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-17 21:21:09 +0700

    update route /barang and /barang/tambah | add toast container

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { Route, Routes } from "react-router"
 import IndexLayout from "./layouts/IndexLayout"
+import ItemList from "./pages/ItemList"
+import ItemAdd from "./pages/ItemAdd"
+import { _ToastContainer } from "./components/toasts/Toast"
 
 function App() {
   return (
-  <Routes>
-    <Route path="/" element={<IndexLayout />}>
-      <Route index element={<h1>Home</h1>} />
-      <Route path='/barang' element={<h1>Barang</h1>} />
-      <Route path='/supplier' element={<h1>Supplier</h1>} />
-      <Route path='/purchase-order' element={<h1>Purchase Order</h1>} />
-      <Route path='/history-barang' element={<h1>History Barang</h1>} />
-    </Route>
-  </Routes>
+    <>
+    <Routes>
+      <Route path="/" element={<IndexLayout />}>
+        <Route index element={<h1>Home</h1>} />
+        <Route path='/barang' element={<ItemList />} />
+        <Route path='/supplier' element={<h1>Supplier</h1>} />
+        <Route path='/purchase-order' element={<h1>Purchase Order</h1>} />
+        <Route path='/history-barang' element={<h1>History Barang</h1>} />
+      </Route>
+      <Route path='/barang/tambah' element={<ItemAdd />}  key='tambah'/>
+    </Routes>
+    <_ToastContainer />
+  </>
 )}
 
 export default App
