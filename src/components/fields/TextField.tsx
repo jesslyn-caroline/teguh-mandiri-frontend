1044acf32e29ea98bcfb27fd0536239118bd86da
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-17 22:07:07 +0700

    add value to input field

--- src/components/fields/TextField.tsx
+++ src/components/fields/TextField.tsx
@@ -1,14 +1,15 @@
 interface Props {
     title: string,
     type: string,
-    id: string
+    id: string,
+    value?: string | number
 }
 
-function TextField({ id, title, type}: Props) {
+function TextField({ id, title, type, value = '' }: Props) {
     return (
     <div className={`w-full flex flex-col gap-y-1`}>
         <label htmlFor={id} className={`text-gray-600 text-sm uppercase font-bold`}>{ title }</label>
-        <input type={type} name={id} id={id} title={title} className={`border border-gray-500 rounded-md text-sm px-3 py-2.5`}/>
+        <input type={type} name={id} id={id} title={title} defaultValue={value} className={`border border-gray-500 rounded-md text-sm px-3 py-2.5`}/>
     </div>
 )}
 
