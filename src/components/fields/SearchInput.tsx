a163d1fdcef9534278653abaf0d76df487d65832
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-19 13:10:25 +0700

    add autofocus

--- src/components/fields/SearchInput.tsx
+++ src/components/fields/SearchInput.tsx
@@ -1,10 +1,11 @@
 interface Props {
     placeholder: string,
     value: string,
-    onChange: (e: any) => void
+    onChange: (e: any) => void,
+    autofocus?: boolean
 }
 
-function SearchInput({ placeholder, value, onChange }: Props) {
+function SearchInput({ placeholder, value, onChange, autofocus=false }: Props) {
     return (
     <input 
         type="text" name="search" id="search" 
@@ -12,8 +13,8 @@ function SearchInput({ placeholder, value, onChange }: Props) {
         value={value} 
         onChange={onChange} 
         className={`w-full border border-gray-400 rounded-md text-sm px-3 py-2.5`}
+        autoFocus={autofocus}
     />
-
 )}
 
 export default SearchInput
