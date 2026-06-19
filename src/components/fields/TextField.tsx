40f7c592e4180002c620739ea9d02e91b648e398
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-19 13:10:58 +0700

    add needLabel to determine input labeled or not

--- src/components/fields/TextField.tsx
+++ src/components/fields/TextField.tsx
@@ -1,16 +1,18 @@
 interface Props {
+    needLabel?: boolean,
     title: string,
     type: string,
     id: string,
-    value?: string | number,
+    value?: string | number | readonly string[],
     onChange: (e: any) => void,
+    onFocus?: () => void
 }
 
-function TextField({ id, title, type, value, onChange }: Props) {
+function TextField({ needLabel=true, id, title, type, value, onChange, onFocus }: Props) {
     return (
     <div className={`w-full flex flex-col gap-y-1`}>
-        <label htmlFor={id} className={`text-gray-600 text-sm uppercase font-bold`}>{ title }</label>
-        <input type={type} name={id} id={id} title={title} defaultValue={value} onChange={onChange} className={`border border-gray-500 rounded-md text-sm px-3 py-2.5`}/>
+        { needLabel && <label htmlFor={id} className={`text-gray-600 text-sm uppercase font-bold`}>{ title }</label> }
+        <input type={type} name={id} id={id} title={title} value={value} onChange={onChange} className={`border border-gray-500 rounded-md text-sm px-3 py-2.5`} onFocus={onFocus}/>
     </div>
 )}
 
